87347754cd7149349ed708e1ee31abe81cdb6673
巫佳溢 <ste5022424@wujiayide-MacBook-Pro.local>
2019-06-25 17:54:33 +0800

    add Insert,Update,Delete

--- index.js
+++ index.js
@@ -6,3 +6,30 @@ const User = require('./models/User')(sequelize, Sequelize);
 User.findAll().then(users => {
     console.log("All users:", JSON.stringify(users, null, 4));
 });
+
+// Insert
+User.create({
+    Name: 'Tom',
+    Telephone: '09097895541s',
+    Age: '20'
+}).then(the => {
+    console.log(`Insert OK >> ID:${the.ID}`);
+});
+
+// Delete
+User.destroy({
+    where: {
+        Name: "Tom"
+    }
+}).then(() => {
+    console.log(`Delete Tom Done`);
+});
+
+// Update
+User.update({ Name: "WuWu" }, {
+    where: {
+        Name: "Wu"
+    }
+}).then(() => {
+    console.log("Update Wu Done");
+});
